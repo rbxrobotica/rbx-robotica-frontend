0c210ee20978f18c77223224073a7a74117f3e14
anthonysfarias <akileslive.farias10@gmail.com>
2024-08-08 23:10:46 -0300

    feat: adicionando loading page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 
 import { cn } from "@/lib/utils";
-import { Suspense } from "react";
 
 const fontSans = FontSans({
   subsets: ["latin"],
